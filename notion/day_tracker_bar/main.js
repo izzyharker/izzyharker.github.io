655afb7dad8e0a47ad54c28658818f37cb9a6c9b
Izzy Harker <harkerisabella@gmail.com>
2024-01-03 20:28:21 -0800

    added day progress bar

---
+++ notion/day_tracker_bar/main.js
@@ -0,0 +1,35 @@
+function daysInMonth(m) {
+    switch (m) {
+        case 0: return 31
+        case 1: return 29
+        case 2: return 31
+        case 3: return 30
+        case 4: return 31
+        case 5: return 30
+        case 6: return 31
+        case 7: return 30
+        case 8: return 31
+        case 9: return 31
+        case 10: return 30
+        case 11: return 31
+    }
+}
+
+function updateBars() {
+    let date = new Date()
+
+    let month = date.getMonth()
+    let day = date.getDate()
+
+    let days_in_month = daysInMonth(month)
+    month += 1
+
+    document.getElementById("daymonth-text").textContent = `Day ${day}/${days_in_month}`
+    document.getElementById("monthyear-text").textContent = `Month ${month}/12`
+
+    document.getElementById("daymonth-inner").style.width = `${100 * day/days_in_month}%`
+    document.getElementById("monthyear-inner").style.width = `${100 * month/12}%`
+    let t = setTimeout(currentTime, 60000)
+}
+
+updateBars()
